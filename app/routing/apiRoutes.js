c5be1e127f83d67103e277587a574c7c2783fca8
Josh VanOverbeke <joshvanoverbeke@gmail.com>
2019-03-05 00:16:56 -0600

    make it better

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,33 +9,33 @@ module.exports = function(app){
         res.json(friendData);
     });
     app.post("/api/friends", function(req, res) {
-        // let inputScores = req.body.scores;
-        // let friendDataScores = friendData.scores;
-        // var closestmatch ={};
-        // let checkmatch = 0;
-        // var closestmatchscore = 100;
-        // function absSum(array1){
-        //     let sum;
-        //     for(let j = 0; j<array1.length; j++){
-        //         sum += Math.abs(array1[j] - 5)
-        //     }
-        //     return sum;
-        // }
-        // function compare(x, y){
-        //     if(x<y){
-        //         closestmatchscore = checkmatch;
-        //         closestmatch = friendData;
-        //     }
-        // }
+        let inputScores = req.body.scores;
+        let friendDataScores = friendData.scores;
+        var closestmatch ={};
+        let checkmatch = 0;
+        var closestmatchscore = 100;
+        function absSum(array1){
+            let sum;
+            for(let j = 0; j<array1.length; j++){
+                sum += Math.abs(array1[j] - 5)
+            }
+            return sum;
+        }
+        function compare(x, y){
+            if(x<y){
+                closestmatchscore = checkmatch;
+                closestmatch = friendData;
+            }
+        }
 
-        // function loopFriendData(friendData){     
-        //     for (let i = 0; i<friendData.length; i++){
+        function loopFriendData(friendData){     
+            for (let i = 0; i<friendData.length; i++){
 
-        //     console.log(checkmatch);
-        //      checkmatch = absSum(inputScores)}
-        // console.log("Friend data", friendData);
-        // console.log("Friend data score", friendData[0].scores);
-        // console.log("Input Score", req.body.scores);
+            console.log(checkmatch);
+             checkmatch = absSum(inputScores)}
+        console.log("Friend data", friendData);
+        console.log("Friend data score", friendData[0].scores);
+        console.log("Input Score", req.body.scores);
    
             // if (checkmatch < closestmatchscore){
             //     closestmatchscore = checkmatch;
